797105dc580c6aecf71322b63fad1f3d5199a19e
riccardo8bits <ricardo.m.barros6@aluno.senai.br>
2025-12-15 07:51:34 -0300

    Projeto final

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy import select
-from models import local_session, Musica, Base, engine, Artista
+from sqlalchemy import select, func
+from models import local_session, Musica, Base, engine, Artista, Albuns, Usuario, Avaliacao
+import random
 
 # garante que o banco e tabela existem
 Base.metadata.create_all(engine)
@@ -13,7 +14,20 @@ app.config['SECRET_KEY'] = 'ricardo007'
 # Página inicial
 @app.route('/')
 def pagina_inicial():
-    return render_template('base.html')
+    db_session = local_session()
+
+    # Buscar todas as músicas
+    sql = (select(Musica.nome, func.count(Avaliacao.id).label("curtidas"))
+           .select_from(Avaliacao)
+            .join(Musica, Musica.id == Avaliacao.id_musica)
+           .group_by(Avaliacao.id_musica, Musica.nome)
+           .order_by(func.count(Avaliacao.id).label("curtidas").desc())
+           .limit(10))
+    musicas = db_session.execute(sql).all()
+    print(musicas)
+
+
+    return render_template('pagina_inicial.html', musicas=musicas)
 
 
 
@@ -24,13 +38,15 @@ def listar_musicas():
     try:
         sql = select(Musica)
         musicas = db_session.execute(sql).scalars().all()
-        return render_template('listar_musicas.html', musicas=musicas)
+        # print('jhgg', musicas)
+        return render_template('listar_musicas.html', var_musicas=musicas)
     except SQLAlchemyError as e:
         print(f'Erro ao consultar músicas: {e}')
         flash('Erro ao carregar músicas', 'error')
         return redirect(url_for('pagina_inicial'))
     except Exception as e:
         db_session.rollback()
+        print(f'Erroxx:  {e}')
         return redirect(url_for('pagina_inicial'))
     finally:
         db_session.close()
@@ -40,19 +56,22 @@ def listar_musicas():
 @app.route('/adicionar_musica', methods=['GET', 'POST'])
 def adicionar_musica():
     if request.method == 'POST':
-        nome = request.form.get('nome')
-        artista = request.form.get('artista')
-        album = request.form.get('album')
-        ano = request.form.get('ano')
-        url_img = request.form.get('url_img')
+        nome_ = request.form.get('nome')
+        artista_ = request.form.get('artista')
+        album_ = request.form.get('album')
+        ano_ = request.form.get('ano')
+        letras_ = request.form.get('letras')
+        url_img_ = request.form.get('url_img')
 
-        if not nome or not artista:
+        print(nome_, artista_, album_, ano_, letras_, url_img_)
+
+        if not nome_ or not artista_:
             flash('Nome e artista são obrigatórios!', 'error')
             return redirect(url_for('adicionar_musica'))
 
         db_session = local_session()
         try:
-            nova = Musica(nome=nome, artista=artista, album=album, ano_lancamento=ano, url_img=url_img)
+            nova = Musica(nome=nome_, artista=artista_, album=album_, ano_lancamento=ano_, letras=letras_, url_img=url_img_)
             db_session.add(nova)
             db_session.commit()
             flash('Música adicionada com sucesso!', 'success')
@@ -61,6 +80,12 @@ def adicionar_musica():
             db_session.rollback()
             flash('Erro ao salvar música!', 'error')
             print(e)
+            return redirect(url_for('listar_musicas'))
+        except Exception as e:
+            db_session.rollback()
+            flash('Erro inesperado ao salvar música!', 'error')
+            print(e)
+            return redirect(url_for('listar_musicas'))
         finally:
             db_session.close()
     return render_template('adicionar_musica.html')
@@ -71,39 +96,51 @@ def adicionar_musica():
 def musica_especifica(id_musica):
     db_session = local_session()
     musica = db_session.get(Musica, id_musica)
+    sql1 = select(func.count(Avaliacao.id_usuario)).where(Avaliacao.id_musica == int(id_musica))
+    avaliacoes = db_session.execute(sql1).scalars().first()
     db_session.close()
     if not musica:
         flash('Música não encontrada.', 'error')
         return redirect(url_for('listar_musicas'))
-    return render_template('musica_especifica.html', musica=musica)
+    return render_template('musica_especifica.html', musica=musica, avaliacoes=avaliacoes)
 
 
 # Excluir música
-@app.route('/musica/<int:id_musica>/deletar', methods=['POST'])
+@app.route('/musica/<int:id_musica>/deletar', methods=['GET', 'POST'])
 def deletar_musica(id_musica):
     db_session = local_session()
-    musica = db_session.get(Musica, id_musica)
+    musica_sql = select(Musica).where(Musica.id == id_musica)
+    musica = db_session.execute(musica_sql).scalars().one_or_none()
     if not musica:
         flash('Música não encontrada.', 'error')
+        db_session.close()
         return redirect(url_for('listar_musicas'))
+
     try:
         db_session.delete(musica)
         db_session.commit()
         flash('Música excluída com sucesso!', 'success')
+        return redirect(url_for('listar_musicas'))
     except Exception as e:
         db_session.rollback()
+        print("erro", e)
         flash('Erro ao excluir música.', 'error')
-        print(e)
     finally:
         db_session.close()
+
     return redirect(url_for('listar_musicas'))
+
+
+
+
+
 @app.route('/artistas')
 def listar_artistas():
     db_session = local_session()
     try:
         sql = select(Artista)
         artistas = db_session.execute(sql).scalars().all()
-        return render_template('listar_artistas.html', artistas=artistas)
+        return render_template('listar_artistas.html', var_artistas=artistas)
     except SQLAlchemyError as e:
         print(f'Erro ao consultar artistas: {e}')
         flash('Erro ao carregar artistas', 'error')
@@ -159,7 +196,7 @@ def artista_especifica(id_artista):
 
 
 # Excluir música
-@app.route('/artista/<int:id_artista>/deletar', methods=['POST'])
+@app.route('/artista/<int:id_artista>/deletar', methods=['POST', 'GET'])
 def deletar_artista(id_artista):
     db_session = local_session()
     artista = db_session.get(Artista, id_artista)
@@ -179,8 +216,235 @@ def deletar_artista(id_artista):
     return redirect(url_for('listar_artistas'))
 
 
+@app.route('/artista/<int:id_artista>/editar_artista', methods=['GET', 'POST'])
+def editar_artista(id_artista):
+    db_session = local_session()
+    artista = db_session.get(Artista, id_artista)
+
+    if not artista:
+        flash("Música não encontrada.", "error")
+        return redirect(url_for('listar_artistas'))
+
+    if request.method == 'POST':
+        artista.nome = request.form.get('nome')
+        artista.sobrenome = request.form.get('sobrenome')
+        artista.data_nascimento = request.form.get('data_nascimento')
+
+        try:
+            db_session.commit()
+            flash("Artista atualizada com sucesso!", "success")
+            return redirect(url_for('artista_especifica', id_artista=id_artista))
+        except Exception as e:
+            db_session.rollback()
+            flash("Erro ao atualizar artista.", "error")
+            print(e)
+        finally:
+            db_session.close()
+
+    db_session.close()
+    return render_template('editar_artista.html', artista=artista)
 
 
+@app.route('/musica/<int:id_musica>/editar_musica', methods=['GET', 'POST'])
+def editar_musica(id_musica):
+    db_session = local_session()
+    musica = db_session.get(Musica, id_musica)
+
+    if not musica:
+        flash("Música não encontrada.", "error")
+        return redirect(url_for('listar_musicas'))
+
+    if request.method == 'POST':
+        musica.nome = request.form.get('nome')
+        musica.artista = request.form.get('artista')
+        musica.album = request.form.get('album')
+        musica.ano_lancamento = request.form.get('ano')
+
+        try:
+            db_session.commit()
+            flash("Música atualizada com sucesso!", "success")
+            return redirect(url_for('musica_especifica', id_musica=id_musica))
+        except Exception as e:
+            db_session.rollback()
+            flash("Erro ao atualizar música.", "error")
+            print(f'erro: {e}')
+            db_session.close()
+            return render_template('editar_musica.html', musica=musica)
+        finally:
+            db_session.close()
+
+    db_session.close()
+    return render_template('editar_musica.html', musica=musica)
+
+
+@app.route('/albuns')
+def listar_albuns():
+    db_session = local_session()
+    try:
+        sql = select(Albuns)
+        albuns = db_session.execute(sql).scalars().all()
+        print("listar albuns", albuns)
+        return render_template('listar_albuns.html', var_albuns=albuns)
+    except SQLAlchemyError as e:
+        print(f'Erro ao consultar albuns: {e}')
+        flash('Erro ao carregar albuns', 'error')
+        db_session.rollback()
+        return redirect(url_for('pagina_inicial'))
+    except Exception as e:
+        db_session.rollback()
+        flash('Erro inesperado', 'error')
+        print(f'Erro ao consultar albuns##: {e}')
+        return redirect(url_for('pagina_inicial'))
+    finally:
+        db_session.close()
+
+
+# Adicionar nova música
+@app.route('/adicionar_album', methods=['GET', 'POST'])
+def adicionar_album():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        artista = request.form.get('artista')
+        ano_lancamento = request.form.get('ano_lancamento')
+        nome_musica = request.form.get('nome_musica')
+        url_img = request.form.get('url_img')
+
+        if not nome or not nome_musica:
+            flash('Nome e nome da musica são obrigatórios!', 'error')
+            return redirect(url_for('adicionar_album'))
+
+        db_session = local_session()
+        try:
+            nova = Albuns(nome=nome, artista=artista, ano_lancamento=ano_lancamento, nome_musica=nome_musica,
+                          url_img=url_img)
+            db_session.add(nova)
+            db_session.commit()
+            flash('albunm adicionado com sucesso!', 'success')
+            return redirect(url_for('listar_albuns'))
+        except SQLAlchemyError as e:
+            db_session.rollback()
+            flash('Erro ao salvar album!', 'error')
+            print(e)
+        finally:
+            db_session.close()
+    return render_template('adicionar_album.html')
+
+
+# Ver detalhes de uma música
+@app.route('/albuns/<int:id_albuns>')
+def album_especifico(id_albuns):
+    db_session = local_session()
+    albuns = db_session.get(Albuns, id_albuns)
+    db_session.close()
+    if not albuns:
+        flash('album não encontrado.', 'error')
+        return redirect(url_for('listar_albuns'))
+    return render_template('album_especifico.html', albuns=albuns)
+
+
+@app.route('/albuns/<int:id_albuns>/deletar', methods=['POST', 'GET'])
+def deletar_albuns(id_albuns):
+    db_session = local_session()
+    albuns = db_session.get(Albuns, id_albuns)
+    if not albuns:
+        flash('album não encontrado.', 'error')
+        return redirect(url_for('listar_albuns'))
+    try:
+        db_session.delete(albuns)
+        db_session.commit()
+        flash('Album excluído com sucesso!', 'success')
+    except Exception as e:
+        db_session.rollback()
+        flash('Erro ao excluir album.', 'error')
+        print(e)
+    finally:
+        db_session.close()
+    return redirect(url_for('listar_albuns'))
+
+
+@app.route('/albuns/<int:id_albuns>/editar_albuns', methods=['GET', 'POST'])
+def editar_albuns(id_albuns):
+    db_session = local_session()
+    albuns = db_session.get(Albuns, id_albuns)
+
+    if not albuns:
+        flash("Música não encontrada.", "error")
+        return redirect(url_for('listar_albuns'))
+
+    if request.method == 'POST':
+        albuns.nome = request.form.get('nome')
+        albuns.artista = request.form.get('artista')
+        albuns.ano_lancamento = request.form.get('ano_lancamento')
+        albuns.nome_musica = request.form.get('nome_musica')
+
+        try:
+            db_session.commit()
+            flash("Album atualizado com sucesso!", "success")
+            return redirect(url_for('album_especifica', id_albuns=id_albuns))
+        except Exception as e:
+            db_session.rollback()
+            flash("Erro ao atualizar album.", "error")
+            print(e)
+        finally:
+            db_session.close()
+
+    db_session.close()
+    return render_template('editar_albuns.html', albuns=albuns)
+
+
+@app.route('/usuario', methods=['GET', 'POST'])
+def usuario():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        if not nome or not email or not senha:
+            flash('Nome,email e senha são obrigatórios!', 'error')
+            return redirect(url_for('usuario'))
+
+        db_session = local_session()
+        try:
+            nova = Usuario(nome=nome, email=email, senha=senha)
+            db_session.add(nova)
+            db_session.commit()
+            flash('Usuario cadastrado com sucesso!', 'success')
+            return redirect(url_for('pagina_inicial'))
+        except SQLAlchemyError as e:
+            db_session.rollback()
+            flash('Erro ao salvar usuario!', 'error')
+            print(e)
+        finally:
+            db_session.close()
+    return render_template('usuario.html')
+
+
+@app.route('/avaliacao/<id_musica>')
+def avaliacao(id_musica):
+    db_session = local_session()
+    try:
+
+        # Fazer um select para obter os ids dos usuários cadastrados
+        sql= select(Usuario.id).where(Usuario.id != Avaliacao.id_usuario)
+        ids_usuarios = db_session.execute(sql).scalars().all()
+        print(ids_usuarios)
+
+        # Com a lista dos ids escolher um aleatóriamente usando random
+        id_usuario = random.choice(ids_usuarios)
+        # -------------------------------------------------------------
+        print("Usuário sorteado:", id_usuario)
+
+        nova = Avaliacao(id_usuario=id_usuario, id_musica=int(id_musica))
+        db_session.add(nova)
+        db_session.commit()
+
+        flash("Música curtida!", "success")
+        return redirect(url_for('listar_musicas'))
+    except SQLAlchemyError as e:
+        db_session.rollback()
+        flash("Erro ao curtir música!", "error")
+        print(e)
+        return redirect(url_for("listar_musicas"))
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 # importar bibliotecas
 import integer
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine, Text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
@@ -19,11 +19,12 @@ class Musica(Base):
     artista = Column(String(60), nullable=False)
     album = Column(String(60))
     ano_lancamento = Column(String(60))
+    letras = Column(Text, nullable=False)
     url_img = Column(String(255), nullable=True)
 
     def __repr__(self):
         return (f"<Musica(id={self.id}, nome='{self.nome}', artista='{self.artista}', "
-                f"album='{self.album}', ano_lancamento='{self.ano_lancamento}'), url_img='{self.url_img}'>")
+                f"album='{self.album}', ano_lancamento='{self.ano_lancamento}', letras='{self.letras}', url_img='{self.url_img}'>")
 
 class Artista(Base):
     __tablename__ = 'artista'
@@ -38,7 +39,44 @@ class Artista(Base):
                 f"data_nascimento='{self.data_nascimento}'), url_img='{self.url_img}'>")
 
 
+class Albuns(Base):
+    __tablename__ = 'albuns'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(60), nullable=False)
+    artista = Column(String(60), nullable=False)
+    ano_lancamento = Column(String(60), nullable=False)
+    nome_musica = Column(String(60), nullable=False)
+    url_img = Column(String(255), nullable=True)
 
-# cria a tabela se ainda não existir
-if __name__ == "__main__":
-    Base.metadata.create_all(engine)
+    def __repr__(self):
+        return (f"<albuns(id={self.id}, nome='{self.nome}', artista='{self.artista}', "
+                f"ano_lancamento='{self.ano_lancamento}'), nome_musica='{self.nome_musica}'>, url_img='{self.url_img}'>")
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(60), nullable=False)
+    email = Column(String(120), nullable=False, unique=True)
+    senha = Column(String(255), nullable=False)
+
+    def __repr__(self):
+        return (
+            f"<Usuario(id={self.id}, nome='{self.nome}', "
+            f"email='{self.email}', senha='{self.senha}')>"
+        )
+
+class Avaliacao(Base):
+    __tablename__ = 'avaliacao'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    id_usuario = Column(Integer, nullable=False)
+    id_musica = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return (
+            f"<Avaliacao(id={self.id}, id_usuario='{self.id_usuario}', id_musica='{self.id_musica}')>")
+
+    # cria a tabela se ainda não existir
+    if __name__ == "__main__":
+        Base.metadata.create_all(engine)
